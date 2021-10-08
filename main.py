9cca8454c315e0519fbe262cb18452c62cd8884b
epkalibbala <58772820+epkalibbala@users.noreply.github.com>
2021-10-08 18:28:16 +0000

    hangman Advanced

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
-word_list = ['Mosquito', 'Umbrella', ]
+import random
+
+word_list = ['mosquito', 'umbrella', 'bus']
+
+choose_word = random.choice(word_list)
+
+progress = ''
+user_input = ''
+correct_letts = ''
+
+for n in range(0,len(choose_word)):
+  progress+='_'
+
+print('Guess the word')
+#print('mosquito'.index('t', 0))
+while (len(choose_word) > len(user_input) or len(choose_word) >= len(correct_letts)):
+  print(progress)
+
+  guess = input("What's your guess? ")
+
+  if guess.lower() in choose_word:
+    correct_letts+=guess
+  else:
+    user_input+=guess
+
+  for letter in correct_letts:
+    if letter != '':
+      p = list(progress)
+      p[choose_word.index(letter, 0)] = letter
+      progress = "".join(p)
